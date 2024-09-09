7eed5c6f9df2e0f16d32ed5b8fdb73c7790aa0cb
ArtemZinukov <zinykov_a@mail.ru>
2024-09-09 19:20:19 +0300

    Настроил серилизатор, чтобы не было путых значений

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Order, OrderProduct
 
-
 class OrderProductSerializer(serializers.ModelSerializer):
     product = serializers.CharField()
 
@@ -11,13 +10,8 @@ class OrderProductSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    products = serializers.ListField(child=OrderProductSerializer(), write_only=True)
+    products = serializers.ListField(child=OrderProductSerializer(), required=True)
 
     class Meta:
         model = Order
         fields = ['id', 'firstname', 'lastname', 'phonenumber', 'address', 'products']
-
-    def validate_products(self, value):
-        if not value:
-            raise serializers.ValidationError('Список продуктов не может быть пустым')
-        return value
